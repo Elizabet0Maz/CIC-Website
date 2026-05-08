76146ae5477baa7e459f25da4fc661d05c2ff190
Elizabet0Maz <elizabethmaznychenko@gmail.com>
2026-05-08 15:10:44 +0400

    Final Website Upload

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const Product = mongoose.model('Product', {
     category: String, // Category ID
     subCategory: String,// subcategory ID
     description: String,
-    price: Number,
     images: [String] // A list to add multiple image links
 });
 
@@ -31,12 +30,122 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/contactus', (req, res) => {
+    res.render('contactus'); 
+});
+
+app.get('/aboutus', (req, res) => {
+    res.render('aboutus'); 
+});
+
 // idea of the code was taken from Route Parameters Setion: https://expressjs.com/en/guide/routing.html
 app.get('/shop/Accessories', async (req, res) => {
     //grabs products in the Accessories category
     const items = await Product.find({ category: 'Accessories' });
     res.render('accessories', { products: items });
 });
+
+app.get('/shop/Handmade', async (req, res) => {
+    //grabs products in the Accessories category
+    const items = await Product.find({ category: 'Handmade' });
+    res.render('handmade', { products: items });
+});
+
+app.get('/shop/Fragrances', async (req, res) => {
+    //grabs products in the Accessories category
+    const items = await Product.find({ category: 'Fragrances' });
+    res.render('fragrance', { products: items });
+});
+
+app.get('/shop/Cosmetics', async (req, res) => {
+    //grabs products in the Accessories category
+    const items = await Product.find({ category: 'Cosmetics' });
+    res.render('cosmetics', { products: items });
+});
+
+
+//SUB CATEGORIES
+
+app.get('/shop/Accessories/Earrings', async (req, res) => {
+    //grabs only the bracelets from the database
+    const items = await Product.find({ 
+        category: 'Accessories', 
+        subCategory: 'Earrings' 
+    });
+    //renders bracelets.ejs
+    res.render('earrings', { products: items });
+});
+
+app.get('/shop/Accessories/Necklaces', async (req, res) => {
+    
+    const items = await Product.find({ 
+        category: 'Accessories', 
+        subCategory: 'Necklaces' 
+    });
+    
+    res.render('necklaces', { products: items });
+});
+
+app.get('/shop/Handmade/Soaps', async (req, res) => {
+  
+    const items = await Product.find({ 
+        category: 'Handmade', 
+        subCategory: 'Soaps' 
+    });
+   
+    res.render('soaps', { products: items });
+});
+
+app.get('/shop/Fragrances/Perfumes', async (req, res) => {
+
+    const items = await Product.find({ 
+        category: 'Fragrances', 
+        subCategory: 'Perfumes' 
+    });
+  
+    res.render('perfumes', { products: items });
+});
+
+app.get('/shop/Fragrances/Perfumed-Oils', async (req, res) => {
+    const items = await Product.find({ 
+        category: 'Fragrances', 
+        subCategory: 'Perfumed Oils' 
+    });
+    res.render('perfumed-oils', { products: items });
+});
+
+app.get('/shop/Fragrances/Home-Fragrances', async (req, res) => {
+    const items = await Product.find({ 
+        category: 'Fragrances', 
+        subCategory: 'Home Fragrances' 
+    });
+    res.render('home-fragrances', { products: items });
+});
+
+app.get('/shop/Fragrances/Deodarants', async (req, res) => {
+    const items = await Product.find({ 
+        category: 'Fragrances', 
+        subCategory: 'Deodarants' 
+    });
+    res.render('deodarants', { products: items });
+});
+
+app.get('/shop/Fragrances/Face-Care', async (req, res) => {
+    const items = await Product.find({ 
+        category: 'Cosmetics', 
+        subCategory: 'Face Care' 
+    });
+    res.render('face-care', { products: items });
+});
+
+app.get('/shop/Fragrances/Body-Care', async (req, res) => {
+    const items = await Product.find({ 
+        category: 'Cosmetics', 
+        subCategory: 'Body Care' 
+    });
+    res.render('body-care', { products: items });
+});
+
 //reference for Route Parameters: https://www.geeksforgeeks.org/node-js/how-to-handle-route-parameters-in-express/
 app.get('/product/:id', async (req, res) => { // Uses async/await to ensure the database responds before the page renders
   const productId = req.params.id;
@@ -50,14 +159,13 @@ app.get('/theadmitaccess27483-087', (req, res) => {
 });
 
 app.post('/theadmitaccess27483-087', async (req, res) => {
-    const { name, category, subCategory, price, description, image1, image2, image3, image4 } = req.body;
+    const { name, category, subCategory, description, image1, image2, image3, image4 } = req.body;
     const imagesArray = [image1, image2, image3, image4].filter(img => img !== "" && img !== undefined);
 
     await new Product({ 
         name, 
         category, 
-        subCategory, 
-        price, 
+        subCategory,
         description, 
         images: imagesArray 
     }).save();
